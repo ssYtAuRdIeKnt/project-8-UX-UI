42c24df5fed0240ab2196304137185f13bfbfb61
YsAuRnOfSiLrAeV <yaroslav.kalinichenko88@gmail.com>
2025-04-09 16:20:59 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import { BrowserRouter } from "react-router-dom";
+import UglyPage from "./components/UglyPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <UglyPage/>
+    </BrowserRouter>
+  );
+};
+
+export default App;
